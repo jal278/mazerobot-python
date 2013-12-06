0057ff63a38c257806d70458549a0e263343fa93
Joel Lehman <joel@n64.csres.utexas.edu>
2013-12-06 14:23:21 -0600

    entropy changes

--- entropy.py
+++ entropy.py
@@ -1,7 +1,8 @@
 import math
 import mazepy
+import random
 from collections import defaultdict
-grid_sz=20
+grid_sz=30
 
 #a function to map a robot's behavior into a grid of niches
 def map_into_grid(robot):
@@ -21,6 +22,13 @@ def distr_entropy(grids,samples):
   entr+=p*math.log(p)
  return -entr
 
+def population_to_grids(pop):
+ grids=defaultdict(float)
+ for k in pop:
+  key=map_into_grid(k)
+  grids[key]+=1
+ return grids 
+
 def mutations_to_grids(robot,mutations):
  grids=defaultdict(float)
  for x in range(mutations):
@@ -31,6 +39,9 @@ def mutations_to_grids(robot,mutations):
   grids[key]+=1
  return grids
 
+def calc_population_entropy(pop):
+ return distr_entropy(population_to_grids(pop),len(pop))
+ 
 def calc_evolvability_entropy(robot,mutations):
  return distr_entropy(mutations_to_grids(robot,mutations),mutations)
 
@@ -38,21 +49,57 @@ def calc_evolvability_cnt(robot,mutations):
  return len(mutations_to_grids(robot,mutations).keys())
 
 evo_fnc = calc_evolvability_entropy
+
 if(__name__=='__main__'):
  #initialize maze stuff with "medium maze" 
  mazepy.mazenav.initmaze("hard_maze_list.txt")
  mazepy.mazenav.random_seed()
 
- #create initial genome
- robot=mazepy.mazenav()
+ population=defaultdict(list)
+ whole_population=[]
+ psize=1500
 
+ for k in range(psize):
+  robot=mazepy.mazenav()
+  robot.init_rand()
+  robot.mutate()
+  robot.map()
+  population[map_into_grid(robot)].append(robot)
+  whole_population.append(robot)
+ solved=False
 
- #initalize it randomly and mutate it once for good measure
- robot.init_rand()
- robot.mutate()
+ evals=psize
+ while not solved:
+  evals+=1
+  if(evals%1000==0):
+   print calc_population_entropy(whole_population)
+
+  keys=population.keys()
+  pniche=random.choice(keys)
+  parent=random.choice(population[pniche])
+  child=parent.copy()
+  child.mutate()
+  child.map()
+
+  if(child.solution()):
+   solved=True
+
+  population[map_into_grid(robot)].append(child)
+  whole_population.append(child)
+
+  to_comp = random.sample(keys,2)    
+  larger=to_comp[0]
+  if(len(population[to_comp[1]])>len(population[to_comp[0]])):
+   larger=to_comp[1]
+  to_kill=random.choice(population[larger])
+
+  population[larger].remove(to_kill)
+  whole_population.remove(to_kill)
+  del to_kill
+  if(len(population[larger])==0):
+   population.pop(larger)
 
  #run genome in the maze simulator
- robot.map()
 
  #calculate evolvability
  print "evolvability:", evo_fnc(robot,400)
